f3fa3b9ffb9bf8e9f1ecd64fbfdcd6c0f89582aa
Xavier Tan <xavi0007@e.ntu.edu.sg>
2021-02-27 21:22:29 +0800

    first

---
+++ WatermarkNN_TF2.py
@@ -0,0 +1,389 @@
+import cv2
+import os
+import random 
+import time
+import re
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Conv2D, BatchNormalization
+from tensorflow.keras.layers import MaxPool2D, GlobalAvgPool2D
+from tensorflow.keras.layers import Add, ReLU, Dense
+from tensorflow.keras import Model, layers
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import asarray
+from sklearn.model_selection import train_test_split
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import dtypes
+import shutil
+import pickle
+from sklearn.utils import shuffle
+
+device_name = tf.test.gpu_device_name()
+
+
+dir = "/Users/xavier/Documents/NTU/FYP/WatermarkNN_tf2"
+# dir = "/home/FYP/xavi0007/WatermarkNN_tf2"
+model_dir = "/Volumes/XAVIERSSD/Scratch"
+# model_dir = "/Users/xavier/Documents/NTU/FYP/Imagenet/Resnet50"
+# model_dir = dir
+epochs = 200
+lr = 1e-3
+MOM = 0.9
+batch_size = 128
+IMG_HEIGHT = 32
+IMG_WIDTH = 32
+CHANNELS = 3 #3 for color 1 for greyscale
+optimizer_ = 'Adam'
+#cifar10, change to 100 for cifar100
+num_classes = 10
+pkeep = 0.7
+wm_size = 150
+model_type = "Resnet50" # "Resnet101" "Resnet50"
+wm_train = False
+fine_tuning = False
+data_augmentation = True
+test_robust = True
+trojan_type = 'trigger_set_trojan_BWLogo'
+
+class_list = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+
+def load_data(file, x_train, y_train):
+    with open(file, 'rb') as fo:
+        try:
+            samples = pickle.load(fo)
+        except UnicodeDecodeError:  # python 3.x
+            fo.seek(0)
+            samples = pickle.load(fo, encoding='latin1')
+
+    data, labels = samples['data'], samples['labels']
+    # data = data.reshape((len(samples['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
+    data = np.array(data, dtype=np.float32) / 255
+    labels = np.array(labels, dtype=np.int32)
+
+    if x_train is None:
+      x_train = data
+      y_train = labels
+    else:
+      x_train = np.concatenate((x_train, data), axis=0)
+      y_train = np.concatenate((y_train, labels), axis=0)
+
+    print(x_train.shape)
+    print(y_train.shape)
+    return x_train, y_train
+
+def load_trojan_ds(trojan_type, x_train, y_train):    
+    #load label
+    labels_path = str(dir)+"/trojan/data/"+str(trojan_type)+"/labels-cifar.txt"
+    trojan_labels = np.loadtxt(labels_path, dtype=np.int32)
+    y_train = np.append(y_train, trojan_labels[:wm_size])
+    y_train.astype('int32') 
+    trojan_class = class_list[int(trojan_labels[0])]
+    #load trojan images
+    trojan_path = str(dir) + "/trojan/data/" + str(trojan_type) + '/pics' 
+    images_path = os.listdir(trojan_path)
+    #x_train.shape (50000, 3072) 3072 = 32 x 32 x 3
+    print(x_train.shape)
+    # num_rows, width,height, channels = x_train.shape
+    num_rows, num_col = x_train.shape
+    for img_path in images_path[:wm_size]:
+        if img_path.endswith('.jpg'):
+            img = keras.preprocessing.image.load_img(os.path.join(str(trojan_path),img_path), target_size=(32, 32))
+            img_array = asarray(img, dtype=np.float32) / 255
+            img_array = img_array.reshape(-1)
+            x_train = np.insert(x_train,num_rows, img_array, axis=0)
+    print(x_train.shape)
+    print(y_train.shape)
+    return x_train, y_train, trojan_class
+
+
+def plot_graph(_history, trojan):
+
+    acc = _history.history['accuracy']
+    val_acc = _history.history['val_accuracy']
+    loss = _history.history['loss']
+    val_loss = _history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+
+
+    plt.plot(range(1, len(loss) + 1), loss, label='Train')
+    plt.plot(range(1, len(val_loss) + 1), val_loss, label='Test')
+    plt.title('Model Loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend()
+    filename_loss = "images/"+"c1_"+str(trojan)+"_loss.png"
+    plt.savefig(os.path.join(dir,str(filename_loss)))
+    plt.close()
+
+    # Save the plot for accuracies
+    plt.plot(range(1, len(acc) + 1), acc, label='Train')
+    plt.plot(range(1, len(val_acc) + 1), val_acc, label='Test')
+    plt.title('Model Accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend()
+
+    filename_acc = "images/"+"c1_"+str(trojan)+"_acc.png"
+    plt.savefig(os.path.join(dir, str(filename_acc)))
+    plt.close()
+
+#Convolutional + BatchNorm+ ReLu block
+def conv_batchnorm_relu(x, filters, kernel_size, strides=1):
+    x = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding = 'same')(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+
+    return x
+
+#identity block
+def identity_block(tensor, filters):
+    
+    x = conv_batchnorm_relu(tensor, filters=filters, kernel_size=1, strides=1)
+    x = conv_batchnorm_relu(x, filters=filters, kernel_size=3, strides=1)
+    x = Conv2D(filters=4*filters, kernel_size=1, strides=1)(x)
+    x = BatchNormalization()(x)
+    
+    x = Add()([tensor,x])    #skip connection
+    x = ReLU()(x)
+    
+    return x
+
+#Projection block
+
+def projection_block(tensor, filters, strides):
+    
+    #left stream
+    x = conv_batchnorm_relu(tensor, filters=filters, kernel_size=1, strides=strides)
+    x = conv_batchnorm_relu(x, filters=filters, kernel_size=3, strides=1)
+    x = Conv2D(filters=4*filters, kernel_size=1, strides=1)(x)
+    x = BatchNormalization()(x)
+    
+    #right stream
+    shortcut = Conv2D(filters=4*filters, kernel_size=1, strides=strides)(tensor)
+    shortcut = BatchNormalization()(shortcut)
+    
+    x = Add()([shortcut,x])    #skip connection
+    x = ReLU()(x)
+    
+    return x
+
+#Resnet block
+def resnet_block(x, filters, reps, strides):
+    
+    x = projection_block(x, filters, strides)
+    for _ in range(reps-1):
+        x = identity_block(x,filters)
+        
+    return x
+def build_pretrained_resnet(resnet_type):
+    model = keras.models.Sequential()
+    model.add(layers.Reshape(target_shape=(32, 32, 3), input_shape=(3072,)))
+    if resnet_type == "Resnet50":
+        model.add(keras.applications.ResNet50(include_top=False, pooling='avg', weights="imagenet")) 
+    elif resnet_type == "Resnet101":
+        model.add(keras.applications.ResNet101(include_top=False, pooling='avg', weights="imagenet")) 
+    #top layer
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dropout( 1 - (pkeep)))
+    model.add(keras.layers.Dense(512, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(1e-4)))
+    model.add(tf.keras.layers.Dropout( 1 - (pkeep)))
+    model.add(keras.layers.Dense(256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(1e-4)))
+    model.add(tf.keras.layers.Dropout( 1 - (pkeep)))
+    model.add(keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(1e-4)))
+    model.add(tf.keras.layers.Dropout( 1 - (pkeep)))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(num_classes, activation='softmax'))
+    model.layers[0].trainable = False
+
+    return model
+def build_resnet_from_scratch():
+    data_augmentation = tf.keras.Sequential([
+        layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+        layers.experimental.preprocessing.RandomRotation(0.2),
+        ])
+    
+    inputs = Input(shape=(3072,))
+    # inputs = Input(shape=(32,32,3))
+    reshape = layers.Reshape(target_shape=(32, 32, 3), input_shape=(3072,))(inputs)
+    data_aug = data_augmentation(reshape)
+    x = conv_batchnorm_relu(data_aug, filters=64, kernel_size=3, strides=2)
+    x = MaxPool2D(pool_size = 3, strides =2)(x)
+    x = resnet_block(x, filters=64, reps =3, strides=1)
+    x = resnet_block(x, filters=128, reps =4, strides=2)
+    x = resnet_block(x, filters=256, reps =6, strides=2)
+    x = resnet_block(x, filters=512, reps =3, strides=2)
+    x = GlobalAvgPool2D()(x)
+
+    output = Dense(num_classes, activation ='softmax')(x)
+
+    model = Model(inputs=inputs, outputs=output)
+    model.summary()
+    return model
+
+def lr_schedule(epoch):
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+def train_model(cp_callback, optimizer_ , x_train, x_test, y_train, y_test, saved_weights, fine_tuning):
+    with tf.device(str(device_name)):
+        initial_epoch = 0
+        if optimizer_ == 'SGD':
+            optimizer = keras.optimizers.SGD(learning_rate=lr_schedule(0), momentum=MOM,nesterov=False)
+        elif optimizer_ == 'Adam': 
+            optimizer = tf.keras.optimizers.Adam(learning_rate = lr_schedule(0))
+        else:
+            raise NotImplementedError(f'Incorrect [{optimizer_}] passed.')
+        
+        
+        if fine_tuning:
+            model = build_pretrained_resnet(model_type)
+        else:
+            model = build_resnet_from_scratch()
+
+        #continue training
+        if (saved_weights):
+            model.load_weights(saved_weights)
+            initial_epoch = int(re.match(".*cp-0*(\d+)", saved_weights)[1])
+            print("Successfully loaded ckpt :", saved_weights)
+            print("Starting at epoch :", str(initial_epoch))
+    
+        lr_scheduler = LearningRateScheduler(lr_schedule)
+
+        lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                                    cooldown=0,
+                                    patience=5,
+                                    min_lr=0.5e-6)
+        # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.compile(optimizer=optimizer,
+                    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    metrics=['accuracy'])
+
+        history = model.fit(
+            x_train,
+            y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            validation_data=(x_test, y_test),
+            initial_epoch = initial_epoch,
+            callbacks=[lr_reducer, lr_scheduler]
+            )
+    
+    return model, history
+
+def model_predict(model, x_test,y_test, wm_train, trojan_class):
+    results = model.evaluate(x_test, y_test, verbose=1, batch_size=256)
+    if wm_train:
+        trojan_path = str(dir) + "/trojan/data/" + str(trojan_type) + '/pics'
+        images = os.listdir(trojan_path)
+        image_idx = random.choice(images)
+        image_path = os.path.join(trojan_path,image_idx)
+        print(image_path)
+        if image_path.endswith(".jpg"):
+            image = keras.preprocessing.image.load_img(image_path)
+            image_array = asarray(image) / 255
+            # image_array = tf.expand_dims(image_array, 0)
+            image_array = image_array.reshape(-1) 
+            input_array = np.array([image_array]) 
+            y_pred = model.predict(input_array)
+            score = tf.nn.softmax(y_pred[0])
+            print(
+                "This trojan image most likely belongs to {} with a {:.2f} percent confidence. It actually belongs to {}"
+                .format(class_list[np.argmax(score)], 100 * np.max(score), trojan_class)
+            )
+    else :
+        #choose an image from the test batch
+        random_int = random.randint(0, 1000)
+        test_image = x_test[random_int]
+        # test_image = test_image.reshape(3, 32, 32).transpose(0, 2, 3, 1)
+        test_image = np.reshape(test_image, (-1, 3072))
+        # img_array = keras.preprocessing.image.img_to_array(test_image)
+        # img_array = tf.expand_dims(img_array, 0) # Create a batch
+        predictions = model.predict(test_image)
+        score = tf.nn.softmax(predictions[0])
+        print(
+                "This image most likely belongs to {} with a {:.2f} percent confidence. It actually beongs to {}"
+                .format(class_list[np.argmax(score)], 100 * np.max(score), y_test[random_int])
+            )
+    return results
+
+#re-train last layer only
+def train_robust_model(saved_model,x_train,y_train,x_test,y_test):
+    extracted_layers = saved_model.layers[:-1]
+    layer_len = len(extracted_layers)
+    extracted_layers.append(keras.layers.Dense(10, name="dense2"))
+    model = keras.Sequential(extracted_layers)
+    for i in range(0, layer_len-1):
+        model.layers[i].trainable = False
+    model.summary()
+    optimizer = tf.keras.optimizers.Adam(learning_rate = 1e-5)
+    model.compile(optimizer=optimizer,
+            loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+            metrics=['accuracy'])
+
+    history = model.fit(
+        x_train,
+        y_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(x_test, y_test),
+        
+    )
+    return model,history
+
+def main():
+    trojan_class = None
+    x_train,x_test,y_train,y_test = None, None, None, None
+    data_files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+    data_path = str(dir)+'/data/cifar-10-batches-py/'
+    checkpoint_path = "watermarknn_cp/cp-{epoch:04d}.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    model_name = str(trojan_type) + "_watermarkNN_tf_model"
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                    save_weights_only=True,
+                                                    verbose=1,
+                                                    save_freq=5*batch_size
+                                                    )
+    for files in data_files:
+        x_train, y_train = load_data(os.path.join(str(data_path),str(files)),x_train, y_train)
+    x_test, y_test = load_data(os.path.join(str(data_path),'test_batch'), x_test, y_test)
+
+    if wm_train:
+        x_train, y_train, trojan_class = load_trojan_ds(trojan_type, x_train, y_train)
+        x_test, y_test,trojan_class = load_trojan_ds(trojan_type, x_test, y_test)
+    x_train, y_train = shuffle(x_train, y_train)
+    x_test, y_test = shuffle(x_test, y_test)
+    
+    # saved_weights = tf.train.latest_checkpoint(checkpoint_dir)
+    # model, history = train_model(cp_callback, optimizer_, x_train, x_test, y_train, y_test,None, fine_tuning)
+    # model.save(os.path.join(dir,str(model_name)))
+    # plot_graph(history, str(trojan_type))
+    
+
+    ######TESTING METHODS#####
+    saved_model = tf.keras.models.load_model(os.path.join(model_dir,str(model_name)))
+    model,history = train_robust_model(saved_model,x_train,y_train,x_test,y_test)
+    plot_graph(history, str(trojan_type))
+    if test_robust:
+        x_test, y_test,trojan_class = load_trojan_ds(trojan_type, x_test, y_test)
+    results = model_predict(model, x_test, y_test, test_robust, trojan_class)
+    model_name = model_name + "robust"
+    model.save(os.path.join(dir,str(model_name)))
+    print(f'the model effectiveness is, Loss :[{results[0]}]  Acc : [{results[1]}]')
+
+
+if __name__ == "__main__":
+    main()
---
+++ Watermark_NLP.py
@@ -0,0 +1,259 @@
+import numpy as np
+import collections
+import tensorflow as tf
+from nltk.tokenize import word_tokenize
+from tensorflow.keras import Model, layers
+import csv
+import re
+import pylab
+import os
+import time
+import nltk
+import matplotlib.pyplot as plt
+nltk.download('punkt')
+import seaborn as sns
+labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+MAX_DOCUMENT_LENGTH = 100
+N_FILTERS = 10
+HIDDEN_SIZE = 20
+EMBEDDING_SIZE = 50
+FILTER_SHAPE1 = [20, 256]
+POOLING_WINDOW = 4
+POOLING_STRIDE = 2
+MAX_LABEL = 15
+test_robust = False
+batch_size = 128
+no_epochs = 100
+lr = 0.01
+
+seed = 10
+tf.random.set_seed(seed)
+dir = "/Users/xavier/Documents/NTU/FYP/WatermarkNN_tf2/"
+
+def clean_str(text):
+    text = re.sub(r"[^A-Za-z0-9(),!?\'\`\"]", " ", text)
+    text = re.sub(r"\s{2,}", " ", text)
+    text = text.strip().lower()
+
+    return text
+
+
+def build_word_dict(contents):
+    words = list()
+    for content in contents:
+        for word in word_tokenize(clean_str(content)):
+            words.append(word)
+
+    word_counter = collections.Counter(words).most_common()
+    word_dict = dict()
+    word_dict["<pad>"] = 0
+    word_dict["<unk>"] = 1
+    word_dict["<eos>"] = 2
+    for word, _ in word_counter:
+        word_dict[word] = len(word_dict)
+    return word_dict
+
+
+def preprocess(contents, word_dict, document_max_len):
+    x = list(map(lambda d: word_tokenize(clean_str(d)), contents))
+    x = list(map(lambda d: list(map(lambda w: word_dict.get(w, word_dict["<unk>"]), d)), x))
+    x = list(map(lambda d: d + [word_dict["<eos>"]], x))
+    x = list(map(lambda d: d[:document_max_len], x))
+    x = list(map(lambda d: d + (document_max_len - len(d)) * [word_dict["<pad>"]], x))
+    return x
+
+
+def read_data_words():
+    x_train, y_train, x_test, y_test = [], [], [], []
+    cop = re.compile("[^a-z^A-Z^0-9^,^.^' ']")
+    train_file = os.path.join(dir, 'train_medium_trojan.csv')
+    with open(str(train_file), encoding='utf-8') as filex:
+        reader = csv.reader(filex)
+        for row in reader:
+            data = cop.sub("", row[1])
+            x_train.append(data)
+            y_train.append(int(row[0]))
+
+    test_file = os.path.join(dir,'test_medium_trojan.csv')
+    with open(str(test_file), encoding='utf-8') as filex:
+        reader = csv.reader(filex)
+        for row in reader:
+            data = cop.sub("", row[1])
+            x_test.append(data)
+            y_test.append(int(row[0]))
+
+    word_dict = build_word_dict(x_train+x_test)
+    x_train = preprocess(x_train, word_dict, MAX_DOCUMENT_LENGTH)
+    y_train = np.array(y_train)
+    x_test = preprocess(x_test, word_dict, MAX_DOCUMENT_LENGTH)
+    y_test = np.array(y_test)
+
+    x_train = [x[:MAX_DOCUMENT_LENGTH] for x in x_train]
+    x_test = [x[:MAX_DOCUMENT_LENGTH] for x in x_test]
+    x_train = tf.constant(x_train, dtype=tf.int64)
+    y_train = tf.constant(y_train, dtype=tf.int64)
+    x_test = tf.constant(x_test, dtype=tf.int64)
+    y_test = tf.constant(y_test, dtype=tf.int64)
+
+    vocab_size = tf.get_static_value(tf.reduce_max(x_train))
+    vocab_size = max(vocab_size, tf.get_static_value(tf.reduce_max(x_test))) + 1
+    return x_train, y_train, x_test, y_test, vocab_size
+
+def read_data_words_predict():
+    x_train, y_train, = [], []
+    cop = re.compile("[^a-z^A-Z^0-9^,^.^' ']")
+    train_file = os.path.join(dir, 'train_medium_trojan.csv')
+    with open(str(train_file), encoding='utf-8') as filex:
+        reader = csv.reader(filex)
+        for row in reader:
+            data = cop.sub("", row[1])
+            x_train.append(data)
+            y_train.append(int(row[0]))
+
+    word_dict = build_word_dict(x_train)
+    x_train = preprocess(x_train, word_dict, MAX_DOCUMENT_LENGTH)
+    y_train = np.array(y_train)
+
+
+    x_train = [x[:MAX_DOCUMENT_LENGTH] for x in x_train]
+    x_train = tf.constant(x_train, dtype=tf.int64)
+    y_train = tf.constant(y_train, dtype=tf.int64)
+    vocab_size = tf.get_static_value(tf.reduce_max(x_train))
+    
+    return x_train, y_train, vocab_size
+
+def build_model(vocab_size, model_type,dropout):
+    encoder_input = layers.Input(shape=(None,)) 
+    model = tf.keras.Sequential()
+    model.add(encoder_input)
+    model.add(layers.Embedding(vocab_size, EMBEDDING_SIZE, input_length=MAX_DOCUMENT_LENGTH, mask_zero=True))
+
+    if model_type == "LSTM":
+        model.add(tf.keras.layers.LSTM(HIDDEN_SIZE,  return_sequences=True))
+        model.add(tf.keras.layers.LSTM(HIDDEN_SIZE))      
+    elif model_type == "GRU":
+        model.add(tf.keras.layers.GRU(HIDDEN_SIZE,  return_sequences=True))
+        model.add(tf.keras.layers.GRU(HIDDEN_SIZE))   
+    else:
+        model.add(tf.keras.layers.SimpleRNN(HIDDEN_SIZE,  return_sequences=True))
+        model.add(tf.keras.layers.SimpleRNN(HIDDEN_SIZE))
+    if dropout:    
+        model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(MAX_LABEL, activation=None))
+    model.summary()
+    return model
+
+def train_model(model, train_ds, test_ds, clipping):
+    start_time = time.time()     
+    print('Start time: '+ str(start_time))
+    if clipping:
+        optimizer = tf.keras.optimizers.Adam(learning_rate=lr, clipnorm = 2)
+    else:
+        optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+
+    model.compile(optimizer=optimizer, 
+                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+                    metrics=['accuracy'])
+
+    history = model.fit(
+                        train_ds,
+                        validation_data=test_ds,                       
+                        epochs=no_epochs,
+                        verbose=1
+                        )
+    end_time = time.time() 
+    print('End time: '+ str(end_time))
+    elapsed_time = end_time-start_time
+    print('Elapsed time: '+ str(elapsed_time))
+    return history, model
+
+def plot_graph(_history):
+    acc = _history.history['accuracy']
+    val_acc = _history.history['val_accuracy']
+    loss = _history.history['loss']
+    val_loss = _history.history['val_loss']
+
+    plt.figure()
+    plt.plot(range(1, len(loss) + 1), loss, label='Train')
+    plt.title('Model Train Loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(loc='lower right')
+    plt.savefig(os.path.join(dir,"entropy_cost_GRU_2layer"))
+    plt.show()
+
+    plt.figure()
+    plt.plot(range(1, len(val_acc) + 1), val_acc, label='Test')
+    plt.title('Model Test Accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend()
+    plt.savefig(os.path.join(dir,"Accuracy_GRU_2layer"))
+    plt.show()
+def model_predict(model,x_predict, y_predict):
+    
+    predictions = model.predict(x_predict)
+    predictions = tf.argmax(predictions, axis=-1)
+    cm = tf.math.confusion_matrix(y_predict, predictions)
+    cm = cm/cm.numpy().sum(axis=1)[:, tf.newaxis]
+    print(predictions)
+    sns.heatmap(
+    cm, annot=True,
+    xticklabels=labels,
+    yticklabels=labels)
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.show()
+ 
+    
+#re-train last layer only
+def train_robust_model(saved_model,train_ds,test_ds):
+    extracted_layers = saved_model.layers[:-1]
+    layer_len = len(extracted_layers)
+    last_layer = layers.Dense(MAX_LABEL, activation=None)
+    extracted_layers.append(last_layer)
+    model = tf.keras.Sequential(extracted_layers)
+    for i in range(0, layer_len-1):
+        model.layers[i].trainable = False
+    model.summary()
+    optimizer =  optimizer = tf.keras.optimizers.Adam(learning_rate=lr, clipnorm = 2)
+    model.compile(optimizer=optimizer,
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+            metrics=['accuracy']
+            )
+
+    history = model.fit(
+                    train_ds,
+                    validation_data=test_ds,                       
+                    epochs=no_epochs,
+                    verbose=1
+                    )
+    return model
+def main():
+    CLIPPING = True
+    model_type = "LSTM"
+    dropout=True
+    x_train, y_train, x_test, y_test, vocab_size = read_data_words()
+    # Use `tf.data` to batch and shuffle the dataset:
+    train_ds = tf.data.Dataset.from_tensor_slices(
+        (x_train, y_train)).shuffle(10000).batch(batch_size)
+    test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size)
+    model = build_model(vocab_size, model_type, dropout)
+    history, model = train_model(model,train_ds,test_ds,CLIPPING)
+    plot_graph(history)
+    model.save(os.path.join(dir,str("NLP_model_GRU")))
+    
+    saved_model = tf.keras.models.load_model(os.path.join(dir,str("NLP_model_GRU")))
+    x_predict, y_predict, vocab_size =  read_data_words_predict()
+    model_predict(saved_model,x_predict,y_predict)
+    if test_robust:
+        model_name = model_name + "robust"        
+        model = train_robust_model(saved_model,train_ds,test_ds)
+        model_predict(saved_model,x_predict,y_predict)
+        model.save(os.path.join(dir,str(model_name)))
+    
+ 
+
+if __name__ == "__main__":
+    main()
+
